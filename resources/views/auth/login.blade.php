39375bbd3fe8ae02e2210783ce9e3545caac4aa5
aldiui <aldijaya280902@gmail.com>
2024-02-23 10:32:00 +0700

    fixed login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,1 +1,69 @@
+@extends('layouts.auth')
 
+@section('title', 'Login')
+
+@push('style')
+    <link rel="stylesheet" href="{{ asset('extensions/sweetalert2/sweetalert2.min.css') }}">
+@endpush
+
+@section('main')
+    <div class="row h-100">
+        <div class="col-lg-5 col-12">
+            <div id="auth-left">
+                <div class="auth-logo">
+                    <a href="index.html"><img src="{{ asset('compiled/svg/logo.svg') }}" alt="Logo"></a>
+                </div>
+                <h1 class="auth-title">Log in.</h1>
+                <p class="auth-subtitle mb-5">Log in with your data that you entered during registration.</p>
+
+                <form id="login" autocomplete="off">
+                    <div class="form-group mb-3">
+                        <label for="email" class="form-label">Email <span class="text-danger">*</span></label>
+                        <input id="email" type="email" class="form-control" name="email">
+                        <small class="invalid-feedback" id="erroremail"></small>
+                    </div>
+                    <div class="form-group mb-3">
+                        <label for="password" class="form-label">Password <span class="text-danger">*</span></label>
+                        <input id="password" type="password" class="form-control" name="password">
+                        <small class="invalid-feedback" id="errorpassword"></small>
+                    </div>
+                    <button type="submit" class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Log in</button>
+                </form>
+                <div class="text-center mt-5 text-lg fs-4">
+                    <p class="text-gray-600">Don't have an account? <a href="#" class="font-bold">Signup</a>.</p>
+                    <p><a class="font-bold" href="#">Forgot password?</a>.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-7 d-none d-lg-block">
+            <div id="auth-right">
+            </div>
+        </div>
+    </div>
+@endsection
+
+@push('scripts')
+    <script src="{{ asset('extensions/sweetalert2/sweetalert2.min.js') }}"></script>
+    <script>
+        $(document).ready(function() {
+            $("#login").submit(function(e) {
+                setButtonLoadingState("#login .btn.btn-primary", true, "Login");
+                e.preventDefault();
+                const url = "{{ route('login') }}";
+                const data = new FormData(this);
+
+                const successCallback = function(response) {
+                    setButtonLoadingState("#login .btn.btn-primary", false, "Login");
+                    handleSuccess(response, null, null, "./admin/user");
+                };
+
+                const errorCallback = function(error) {
+                    setButtonLoadingState("#login .btn.btn-primary", false, "Login");
+                    handleValidationErrors(error, "login", ["email", "password"]);
+                };
+
+                ajaxCall(url, "POST", data, successCallback, errorCallback);
+            });
+        });
+    </script>
+@endpush
--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -1,1 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title') - {{ config('app.name') }}</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="shortcut icon" href="{{ asset('compiled/svg/favicon.svg') }}') }}" type="image/x-icon">
+    <link rel="stylesheet" href="{{ asset('compiled/css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('compiled/css/iconly.css') }}">
+    <link rel="stylesheet" href="{{ asset('/compiled/css/auth.css') }}">
+    @stack('style')
+</head>
+
+<body>
+    <script src="{{ asset('static/js/initTheme.js') }}"></script>
+    <div id="auth">
+        @yield('main')
+    </div>
+    <script src="{{ asset('extensions/jquery/jquery.js') }}"></script>
+    <script src="{{ asset('compiled/js/app.js') }}"></script>
+    @stack('scripts')
+    <script src="{{ asset('static/js/custom.js') }}"></script>
+</body>
+
+</html>
