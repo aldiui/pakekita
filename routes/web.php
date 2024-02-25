21aa9668941ec728f64e1cf4b748997b8287ec71
aldiui <aldijaya280902@gmail.com>
2024-02-25 17:58:16 +0700

    update kasir

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
  */
 
 Route::get('/', function () {
-    return redirect("/");
+    return redirect("/login");
 });
 
 Route::match(['get', 'post'], '/login', [App\Http\Controllers\AuthController::class, 'login'])->name('login');
