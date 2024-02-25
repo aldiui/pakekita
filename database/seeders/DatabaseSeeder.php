7b6c649a2a3a1bb4b9e6afd3dced017c9a6e762b
aldiui <aldijaya280902@gmail.com>
2024-02-25 22:18:25 +0700

    update modal select

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -53,5 +53,46 @@ class DatabaseSeeder extends Seeder
         ];
 
         DB::table('units')->insert($unitData);
+
+        $menuData = [
+            ['nama' => 'Nasi Goreng', 'kategori_id' => '1', 'image' => 'makanan.webp', 'harga' => '10000'],
+            ['nama' => 'Ayam Goreng', 'kategori_id' => '1', 'image' => 'makanan.webp', 'harga' => '12000'],
+            ['nama' => 'Mie Goreng', 'kategori_id' => '1', 'image' => 'makanan.webp', 'harga' => '11000'],
+            ['nama' => 'Sate Ayam', 'kategori_id' => '1', 'image' => 'makanan.webp', 'harga' => '15000'],
+            ['nama' => 'Bakso', 'kategori_id' => '1', 'image' => 'makanan.webp', 'harga' => '13000'],
+            ['nama' => 'Soto Ayam', 'kategori_id' => '1', 'image' => 'makanan.webp', 'harga' => '14000'],
+            ['nama' => 'Nasi Kuning', 'kategori_id' => '1', 'image' => 'makanan.webp', 'harga' => '12000'],
+            ['nama' => 'Rendang', 'kategori_id' => '1', 'image' => 'makanan.webp', 'harga' => '20000'],
+            ['nama' => 'Gado-Gado', 'kategori_id' => '1', 'image' => 'makanan.webp', 'harga' => '15000'],
+            ['nama' => 'Capcay', 'kategori_id' => '1', 'image' => 'makanan.webp', 'harga' => '13000'],
+            ['nama' => 'Ikan Bakar', 'kategori_id' => '1', 'image' => 'makanan.webp', 'harga' => '18000'],
+            ['nama' => 'Sayur Asem', 'kategori_id' => '1', 'image' => 'makanan.webp', 'harga' => '12000'],
+            ['nama' => 'Rawon', 'kategori_id' => '1', 'image' => 'makanan.webp', 'harga' => '16000'],
+            ['nama' => 'Sambal Goreng', 'kategori_id' => '1', 'image' => 'makanan.webp', 'harga' => '17000'],
+            ['nama' => 'Es Teh Manis', 'kategori_id' => '2', 'image' => 'minuman.webp', 'harga' => '5000'],
+            ['nama' => 'Es Jeruk', 'kategori_id' => '2', 'image' => 'minuman.webp', 'harga' => '6000'],
+            ['nama' => 'Es Campur', 'kategori_id' => '2', 'image' => 'minuman.webp', 'harga' => '8000'],
+            ['nama' => 'Es Cincau', 'kategori_id' => '2', 'image' => 'minuman.webp', 'harga' => '7000'],
+            ['nama' => 'Es Doger', 'kategori_id' => '2', 'image' => 'minuman.webp', 'harga' => '10000'],
+            ['nama' => 'Es Buah', 'kategori_id' => '2', 'image' => 'minuman.webp', 'harga' => '12000'],
+            ['nama' => 'Es Teler', 'kategori_id' => '2', 'image' => 'minuman.webp', 'harga' => '11000'],
+            ['nama' => 'Es Kelapa Muda', 'kategori_id' => '2', 'image' => 'minuman.webp', 'harga' => '9000'],
+            ['nama' => 'Es Kopyor', 'kategori_id' => '2', 'image' => 'minuman.webp', 'harga' => '15000'],
+            ['nama' => 'Es Campina', 'kategori_id' => '2', 'image' => 'minuman.webp', 'harga' => '13000'],
+            ['nama' => 'Es Blewah', 'kategori_id' => '2', 'image' => 'minuman.webp', 'harga' => '8000'],
+            ['nama' => 'Es Kacang Hijau', 'kategori_id' => '2', 'image' => 'minuman.webp', 'harga' => '7000'],
+            ['nama' => 'Es Selendang Mayang', 'kategori_id' => '2', 'image' => 'minuman.webp', 'harga' => '10000'],
+            ['nama' => 'Es Degan', 'kategori_id' => '2', 'image' => 'minuman.webp', 'harga' => '6000'],
+        ];
+
+        DB::table('menus')->insert($menuData);
+
+        $pembayaranData = [
+            ['nama' => 'OVO', 'jenis' => 'E-Wallet', 'no_rekening' => '08'],
+            ['nama' => 'GOPAY', 'jenis' => 'E-Wallet', 'no_rekening' => '08'],
+            ['nama' => 'DANA', 'jenis' => 'E-Wallet', 'no_rekening' => '08'],
+        ];
+
+        DB::table('pembayarans')->insert($pembayaranData);
     }
 }
